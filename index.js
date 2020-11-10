8fb8ed1e034f65849191099dc08eac3095f818a5
Paul Craig <paul_craig_16@hotmail.com>
2020-11-10 00:35:01 -0500

    Update returned text to verify deployment worked

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get("/", (req, res) => {
   const name = process.env.NAME || "World"
-  res.send(`Hello ${name}!`)
+  res.send(`Hello ${name}! Updated 03:35.`)
 })
 
 const port = process.env.PORT || 8080
